382da8dd9bbb83f664e4a9afc13bae486f3c9d66
Nick Galis <nickgalis123@gmail.com>
2023-10-27 13:20:47 -0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    import javax.swing.*;
+    public static void main(String[] args) {
+        int screenWidth = 800;
+        int screenHeight = screenWidth;
+
+        JFrame frame = new JFrame("Snake");
+    }
+}
