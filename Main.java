6f79c846d3e9b5559c420c14fec243f882103349
Nick Galis <nickgalis123@gmail.com>
2023-10-27 13:23:19 -0400

    error fixed

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
-
 public class Main {
-    import javax.swing.*;
+
     public static void main(String[] args) {
         int screenWidth = 800;
         int screenHeight = screenWidth;
