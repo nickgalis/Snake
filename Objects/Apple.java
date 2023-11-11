ddaa46e26f1418979a0210217569208905ee748a
Nick Galis <nickgalis123@gmail.com>
2023-11-11 15:12:26 -0500

    Apple.java created

---
+++ Objects/Apple.java
@@ -0,0 +1,18 @@
+package Objects;
+
+import Game.Main;
+
+public class Apple {
+    public int x;       //x coordinate
+    public int y;       //y coordinate
+
+
+    public Apple(int startX, int startY) {  
+        x = startX;
+        y = startY; 
+    }
+
+    public void setPosition() {       //changes position of apple
+        ;  //placeholder for now
+    }
+}
