a8f40c489084be53a4d63f127249d9b1fb737d54
Nick Galis <nickgalis123@gmail.com>
2023-11-11 15:16:41 -0500

    changes made to Main

--- Game/Main.java
+++ Game/Main.java
@@ -11,9 +11,6 @@ import java.awt.event.KeyEvent;
 public class Main {
 
     public static void main(String[] args) {
-        int screenWidth = 800;
-        int screenHeight = screenWidth;
-
-        JFrame frame = new JFrame("Snake");
+        ;            //placeholder
     }
 }
