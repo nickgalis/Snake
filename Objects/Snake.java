4cacb9c745caace80c814ce0c4522b2059001e44
Nick Galis <nickgalis123@gmail.com>
2023-11-13 22:11:50 -0500

    variables created

--- Objects/Snake.java
+++ Objects/Snake.java
@@ -3,5 +3,23 @@ package Objects;
 import Game.Main;
 
 public class Snake {
+
+    Game main;
+
+    public int snakeX[] = new int[main.WIDTH * main.HEIGHT];
+    public int snakeY[] = new int[main.WIDTH * main.HEIGHT];
+    int length = 2;
+
+    public Snake(int x0, int y0, int x1, int y1) {
+        snakeX[0] = x0;
+        snakeY[0] = y0;
+        snakeX[1] = x1;
+        snakeY[1] = y1;
+    }
+
+    public void move() {
+        ;   //placeholder
+    }
     
+
 }
