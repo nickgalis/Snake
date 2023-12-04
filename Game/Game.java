9a46cf64cf19dfaab12cc057f4a4ac13eae15c19
Nick Galis <48725456+nickgalis@users.noreply.github.com>
2023-12-04 11:59:00 -0500

    Update Game.java

--- Game/Game.java
+++ Game/Game.java
@@ -9,15 +9,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
  
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-public class Main {
 
-    public static void main(String[] args) {
+public class Game extends JPanel implements ActionListener {
+  public static void main(String[] args) {
         ;            //placeholder
     }
 }
